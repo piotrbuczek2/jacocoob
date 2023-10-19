79d09df44305fdf8b630db7f03010d31df75d8bc
piotrbuczek2 <97125424+piotrbuczek2@users.noreply.github.com>
2023-10-19 20:58:20 +0200

    Add files via upload

---
+++ src/test/java/pl/tests/calculator/CalculatorTestII.java
@@ -0,0 +1,25 @@
+package pl.tests.calculator;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTestII {
+
+    private Calculator calculator;
+
+    @BeforeEach
+    public void setup(){
+        calculator=new Calculator();
+    }
+
+    @Test
+    void add() {
+        int result = calculator.add(3,5);
+        assertEquals(8,result);
+    }
+
+
+
+}
